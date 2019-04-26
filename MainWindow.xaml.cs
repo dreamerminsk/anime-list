d140241a7d088f729e0d6c14bdcfe841d9084114
KCH <tutmirage@gmail.com>
2019-04-26 21:54:51 +0300

    parsing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,33 @@ using System.Windows.Shapes;
 
 namespace AnimeList
 {
+
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private List<AnimeInfo> items = new List<AnimeInfo>();
+
         public MainWindow()
         {
             InitializeComponent();
+            lvItems.ItemsSource = items;
+            Load();
+        }
+
+        private async void Load()
+        {
+            HtmlWeb web = new HtmlWeb();
+            web.OverrideEncoding = Encoding.GetEncoding("Windows-1251");
+            var page = web.Load(@"http://nnmclub.to/forum/portal.php?c=1");
+            var refs = page.DocumentNode.SelectNodes("//table[contains(@class, \"pline\")]//a[contains(concat(\" \", normalize-space(@class), \" \"), \" pgenmed \")]");
+            foreach (var rf in refs)
+            {
+                items.Add(new AnimeInfo { Title = rf.InnerText });
+            }
         }
     }
 }
